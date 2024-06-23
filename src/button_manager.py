f94736443c558861829ada9f73695b1ef1c8b4e3
FlamoDynamo <150678985+FlamoDynamo@users.noreply.github.com>
2024-06-23 16:40:49 +0700

    Update button_manager.py

--- src/button_manager.py
+++ src/button_manager.py
@@ -1,4 +1,4 @@
-# button_manager.py
+'''# button_manager.py
 
 from machine import Pin
 import time
@@ -88,4 +88,52 @@ class ButtonManager:
                         if self.on_button2_double_click:
                             self.on_button2_double_click()
                 self.button2_last_pressed_time = time.ticks_ms()
-                self.button2_pressed_time = 0
+                self.button2_pressed_time = 0'''
+
+class ButtonManager:
+    def __init__(self, button1_pin, button2_pin, button3_pin, long_press_duration=1.0, double_click_interval=0.3):
+        self.button1 = Pin(button1_pin, Pin.IN, Pin.PULL_UP)
+        self.button2 = Pin(button2_pin, Pin.IN, Pin.PULL_UP)
+        self.button3 = Pin(button3_pin, Pin.IN, Pin.PULL_UP)
+        self.long_press_duration = long_press_duration
+        self.double_click_interval = double_click_interval
+
+        self.button_states = {
+            1: {"pressed_time": 0, "last_pressed_time": 0},
+            2: {"pressed_time": 0, "last_pressed_time": 0},
+            3: {"pressed_time": 0, "last_pressed_time": 0}
+        }
+
+        self.callbacks = {
+            1: {"press": None, "long_press": None, "double_click": None},
+            2: {"press": None, "long_press": None, "double_click": None},
+            3: {"press": None, "long_press": None, "double_click": None}
+        }
+
+    def set_callbacks(self, button, on_press=None, on_long_press=None, on_double_click=None):
+        self.callbacks[button]["press"] = on_press
+        self.callbacks[button]["long_press"] = on_long_press
+        self.callbacks[button]["double_click"] = on_double_click
+
+    def check_events(self):
+        for button_num, button in enumerate([self.button1, self.button2, self.button3], 1):
+            self._check_button_events(button_num, button)
+
+    def _check_button_events(self, button_num, button):
+        state = self.button_states[button_num]
+        if button.value() == 0:  # Button pressed
+            if state["pressed_time"] == 0:
+                state["pressed_time"] = time.ticks_ms()
+            if time.ticks_diff(time.ticks_ms(), state["pressed_time"]) >= self.long_press_duration * 1000:
+                if self.callbacks[button_num]["long_press"]:
+                    self.callbacks[button_num]["long_press"]()
+        else:  # Button released
+            if state["pressed_time"] != 0:
+                if time.ticks_diff(time.ticks_ms(), state["pressed_time"]) < self.long_press_duration * 1000:
+                    if self.callbacks[button_num]["press"]:
+                        self.callbacks[button_num]["press"]()
+                    if time.ticks_diff(time.ticks_ms(), state["last_pressed_time"]) <= self.double_click_interval * 1000:
+                        if self.callbacks[button_num]["double_click"]:
+                            self.callbacks[button_num]["double_click"]()
+                state["last_pressed_time"] = time.ticks_ms()
+                state["pressed_time"] = 0
